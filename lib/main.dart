49614af83dc64763cbfff4d1cff47b5eef3a16f7
ColleSerre <61471511+ColleSerre@users.noreply.github.com>
2020-06-18 00:11:11 +0200

    finished full text feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'Manifesto.dart';
 
 void main() => runApp(new MyApp());
 
@@ -41,14 +42,48 @@ class FullText extends StatelessWidget {
         initialPage:
             1), // TODO: Replace 1 by the first page ; set children to be all the other pages using a function ; decide side scrolling vs vertical scrolling
   );
-  static const text1 =
-      """A spectre is haunting Europe – the spectre of communism. All the powers of old Europe have entered into a holy alliance to exorcise this spectre: Pope and Tsar, Metternich and Guizot, French Radicals and German police-spies. Where is the party in opposition that has not been decried as communistic by its opponents in?""";
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: appBar("Full Text"),
       drawer: drawerMenu(context),
-      body: Column(),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                height: size.height / 1.20,
+                width: size.width / 1.30,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.orange[200], width: 4),
+                    borderRadius: BorderRadius.circular(25)),
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    children: <Widget>[
+                      mainTitle(),
+                      paragraph(paragraph1),
+                      header(header2),
+                      paragraph(paragraph2),
+                      header(header3),
+                      paragraph(paragraph3),
+                      header(header4),
+                      paragraph(paragraph4),
+                      header(header5),
+                      paragraph(paragraph5),
+                      coolPunchline()
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -145,4 +180,34 @@ Manifesto""",
       ],
     ),
   );
+}
+
+Widget header(String arg) {
+  return Container(
+    padding: EdgeInsets.all(9),
+    child: Text(
+      arg,
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    ),
+  );
+}
+
+Widget paragraph(String arg) {
+  return Container(
+    padding: EdgeInsets.all(9),
+    child: Text(arg, style: TextStyle(fontSize: 20)),
+  );
+}
+
+Widget mainTitle() {
+  return Text(
+    header1,
+    textAlign: TextAlign.center,
+    style: GoogleFonts.cinzel(fontWeight: FontWeight.bold, fontSize: 30),
+  );
+}
+
+Widget coolPunchline(){
+  return Text(ultraCoolPunchline, textAlign: TextAlign.center,style: TextStyle(fontSize: 30));
 }
